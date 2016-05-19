13133996b8c0ac25da9888070ff2dbd640b48fe9
Norbert Szucs <szucs.norbert@outlook.com>
2016-05-18 21:47:01 -0700

    api routes and endpoints for images ready

--- models/img.js
+++ models/img.js
@@ -9,6 +9,33 @@ var imgSchema = new mongoose.Schema({
   title: { type: String }
 });
 
+imgSchema.statics.getAll = function(cb){
+  Img.find({}, (err, imgs) => {
+    cb(err, imgs)
+  })
+}
+
+imgSchema.statics.getOneById = function(imgId, cb){
+  Img.findById(imgId, (err, img) => {
+    cb(err, img);
+  })
+}
+
+imgSchema.statics.createOne = function(imgObj, cb){
+  imgObj.createdAt = new Date();
+  var img = new Img(imgObj);
+  img.save(cb);
+}
+
+imgSchema.statics.deleteOneById = function(imgId, cb){
+  Img.findByIdAndRemove(imgId, cb);
+}
+
+imgSchema.statics.editOneById = function(imgId, newImgObj, cb) {
+  Img.findByIdAndUpdate(imgId, { $set: newImgObj }, {new: true}, cb);
+}
+
+
 var Img = mongoose.model('Img', imgSchema);
 
 module.exports = Img;
